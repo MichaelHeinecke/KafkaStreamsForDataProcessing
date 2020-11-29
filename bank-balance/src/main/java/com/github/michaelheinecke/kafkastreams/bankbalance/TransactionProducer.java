fa504ea6e1cbdc7f92098dbfa8b8f4bf6c40d5e6
Michael Heinecke <michael.heinecke@booking.com>
2020-11-29 17:09:41 +0100

    Clean-up code after refactoring

--- bank-balance/src/main/java/com/github/michaelheinecke/kafkastreams/bankbalance/TransactionProducer.java
+++ bank-balance/src/main/java/com/github/michaelheinecke/kafkastreams/bankbalance/TransactionProducer.java
@@ -1,7 +1,5 @@
 package com.github.michaelheinecke.kafkastreams.bankbalance;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.kafka.clients.producer.KafkaProducer;
